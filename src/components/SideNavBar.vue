2f36b08ca753c37ce2663506e11b87ae51c89a56
“rdguzmanjr” <jr.dguzman@gmail.com>
2024-03-21 01:04:52 +0800

    style changes

--- src/components/SideNavBar.vue
+++ src/components/SideNavBar.vue
@@ -20,13 +20,13 @@ defineExpose({toggleOpen})
 
   <div class="flex flex-row justify-between items-center mx-3 my-3">
     <div class="relative inline-block text-left ">
-      <button @click="toggleMenu" class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-gray-100 focus:ring-greentool"> 
+      <button @click="toggleMenu" class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-gray-100 focus:ring-greentool"> 
         Tools
         <svg class="h-5 w-5 ml-2" :class="toggleOpen?'rotate-180':'rotate-0'" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
         </svg>
       </button>
-      <div v-show="toggleOpen" class="origin-top-left absolute left-0 mt-2 w-56 rounded-2xl shadow-lg bg-white ring-1 ring-black ring-opacity-5 px-2 py-2">
+      <div v-show="toggleOpen" class="origin-top-left absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 px-2 py-2">
         <RouterLink :to="formatToolText(tool)" class="block px-4 py-2 text-sm text-gray-700 hover:bg-greentool hover:text-white rounded-md" v-for="tool in tools" :key="tool">{{tool}}</RouterLink>
       </div>
     </div>
--- src/views/TransformOrigin.vue
+++ src/views/TransformOrigin.vue
@@ -97,7 +97,7 @@ const emit = defineEmits(['closeMenuBar'])
         <span class="block text-white">Drop your image files here</span>
         </div>
     </div>
-    <button v-if="transImages.length!=0" class="mb-12 self-center w-32 flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-600 rounded-2xl hover:bg-green-500" @click="handleRefresh">
+    <button v-if="transImages.length!=0" class="mb-12 self-center w-32 flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-greentool ring-1 ring-transparent rounded-md hover:ring-white" @click="handleRefresh">
         <svg class="w-5 h-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
         </svg>
