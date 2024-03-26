91baf420873ed56791515d2dab410285403a7d8b
“rdguzmanjr” <jr.dguzman@gmail.com>
2024-03-26 22:19:02 +0800

    fix aws error

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,48 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import commonjs from '@rollup/plugin-commonjs'
 
+export const updateCommonjsPlugin = () => {
+  const commonJs22 = commonjs({
+    include: [/node_modules/],
+    extensions: [".js", ".cjs"],
+    strictRequires: true,
+  });
+
+  return {
+    name: "new-common-js",
+    options(rawOptions) {
+      const plugins = Array.isArray(rawOptions.plugins)
+        ? [...rawOptions.plugins]
+        : rawOptions.plugins
+        ? [rawOptions.plugins]
+        : [];
+
+      const index = plugins.findIndex(
+        (plugin) => plugin && plugin.name === "commonjs"
+      );
+      if (index !== -1) {
+        plugins.splice(index, 1, commonJs22);
+      }
+
+      const nextConfig = { ...rawOptions, plugins };
+      return commonJs22.options.call(this, nextConfig);
+    },
+  };
+};
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(),updateCommonjsPlugin()
   ],
   define:{ //aws
     global:{},
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
   }
 })
+
